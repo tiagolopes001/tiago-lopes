952d28f2b177add57a85ab72c156b5cbf13a5a7b
Tiago lopes <tavaresxboy@gmail.com>
2017-11-17 21:13:54 -0300

    pagina inicial finalizada

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch')
+
+gulp.task('sass', function () {
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.sass', ['sass']);
+});
